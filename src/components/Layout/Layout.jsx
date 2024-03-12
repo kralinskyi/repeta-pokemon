676cd366cafd997e57981dba302980f1765c2334
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-03-12 14:35:42 +0200

    copypast success

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import css from "./Layout.module.css";
 
-// eslint-disable-next-line react/prop-types
 export const Layout = ({ children }) => {
   return <main className={css.container}>{children}</main>;
 };
